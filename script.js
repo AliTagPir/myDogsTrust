a2c95c1fef23647a9b99a22e251f3d6d3775eb2b
alitagpir <alitagpir@gmail.com>
2021-11-02 10:44:46 +0000

    update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 "use strict"
 
-let dogs=[]
 
-dogs.push({name:"Fluffy",Breed:"Poodle",Age:2,picture:"/img/"})
-dogs.push({name:"Otto",Breed:"Chiwowa",Age:1,picture:"/img/"})
-dogs.push({name:"Alfie",Breed:"French Bulldog",Age:8,picture:"/img/"})
-dogs.push({name:"Roger",Breed:"pug",Age:5,picture:"/img/" })
-dogs.push({name:"Steven",Breed:"Alsation",Age:1,picture:"/img/"})
-dogs.push({name:"Francis",Breed:"Shibu",Age:4,picture:"/img/"})
+let dogs=[]
+function setUpDogs(){
+    dogs.push(new Dog("Fluffy","Poodle",10,"/img/"))
+    dogs.push(new Dog("Otto","Chiwowa",13,"/img/"))
+    dogs.push(new Dog("Alfie","French Bulldog",38,"/img/"))
+    dogs.push(new Dog("Roger","pug",3,"/img/"))
+    dogs.push(new Dog("Steven","Alsation",8,"/img/"))
+    dogs.push(new Dog("Francis","Shibu",24,"/img/"))
+}
 
 //6 dogs - on 300*400 pixel cards (with nice padding and margins, and a nice background colour)
 // An image for each dog
@@ -15,33 +17,52 @@ dogs.push({name:"Francis",Breed:"Shibu",Age:4,picture:"/img/"})
 // Add an age property for every dog
 // add an 'Adopt Now' button on each dog
 
-for(let i=0;i<dogs.length;i++){
-   
-    let card = document.createElement("div")//.style.padding= 50 +"px"
-    document.getElementById("cardHolder").appendChild(card)
-    card.classList.add("card")
+function showDogs(){
+    for(let i=0;i<dogs.length;i++){
     
-    let dogName=document.createElement("h1")
-    card.appendChild(dogName)
-    dogName.innerHTML=dogs[i].name
-    
-    let dogBreed=document.createElement("h2")
-    card.appendChild(dogBreed)
-    dogBreed.innerHTML=dogs[i].Breed
-    
-    let dogAge=document.createElement("h3")
-    card.appendChild(dogAge)
-    dogAge.innerHTML=dogs[i].Age
+        let card = document.createElement("div")//.style.padding= 50 +"px"
+        document.getElementById("cardHolder").appendChild(card)
+        card.classList.add("card")
+        
+        let dogName=document.createElement("h1")
+        card.appendChild(dogName)
+        dogName.innerHTML=dogs[i].name
+        
+        let dogBreed=document.createElement("h2")
+        card.appendChild(dogBreed)
+        dogBreed.innerHTML=dogs[i].breed
+        
+        let dogAge=document.createElement("h3")
+        card.appendChild(dogAge)
+        dogAge.innerHTML=dogs[i].dogAge()
 
-    let dogPic=document.createElement("img")
-    card.appendChild(dogPic)
-    dogPic.innerHTML=dogs[i].picture
-    dogPic.src=dogs[i].picture+dogs[i].name +".jpg"
-    dogPic.classList.add("dogPic")
+        let dogPic=document.createElement("img")
+        card.appendChild(dogPic)
+        dogPic.innerHTML=dogs[i].picture
+        dogPic.src=dogs[i].picture+dogs[i].name +".jpg"
+        dogPic.classList.add("dogPic")
 
-    let adopt=document.createElement("button")
-    adopt.innerHTML = "adopt now"
-    card.appendChild(adopt)
-    adopt.classList.add("adoptButton")
+        let adopt=document.createElement("button")
+        adopt.innerHTML = "adopt now"
+        card.appendChild(adopt)
+        adopt.classList.add("adoptButton")
+
+    }
+}
+
+class Dog{
+    constructor(name,breed,age,picture){
+    this.name=name
+    this.breed=breed
+    this.age=age
+    this.picture=picture
+    }
+    dogAge(){
+        return Math.floor(this.age/12 )+" Years "+this.age%12+" months"
+    }
+    
 
 }
+
+setUpDogs()
+showDogs()
